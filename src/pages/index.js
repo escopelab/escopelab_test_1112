a4f6ae855bc1e0d5afebb77b1254dde96c753095
escopelab <escopelab@gmail.com>
2023-11-12 20:25:52 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
-        ctaText={'shop now'}
+        title={'New Collection'}
+        subtitle={'Discover Our New Collection'}
+        ctaText={'learn more'}
         ctaAction={goToShop}
       />
 
